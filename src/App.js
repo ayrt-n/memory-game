b3c3c05679174c3726b3b518f9c607865eb92d25
ayrton <ayrton.parkinson1@gmail.com>
2022-10-02 14:39:45 -0400

    Add scoreboard component to App

--- src/App.js
+++ src/App.js
@@ -1,10 +1,24 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import Game from './components/Game';
+import Scoreboard from './components/Scoreboard';
 
 function App() {
+  const [score, setScore] = useState(0);
+  const [highscore, setHighscore] = useState(0);
+
+  useEffect(() => {
+    if (score > highscore) {
+      setHighscore(score);
+    }
+  }, [score]);
+
   return (
     <div className="App">
-      <Game />
+      <Game
+        incrementScore={() => setScore(score + 1)}
+        resetScore={() => setScore(0)}
+      />
+      <Scoreboard score={score} highscore={highscore} />
     </div>
   );
 }
